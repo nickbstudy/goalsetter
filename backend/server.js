dabfdb913e10050190aa1372fc41086a83e9b163
nickbstudy <nickb.study@hotmail.com>
2022-08-06 12:08:07 +1200

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
-
+const connectDB = require('./config/db')
 const app = express()
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
