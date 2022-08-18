4ab3fbee407ee183905b2ec315d52240f37a7246
Nick Ubuntu <nickb.study@hotmail.com>
2022-08-19 11:12:44 +1200

    Register Login and Logout working

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
 import { FaSignInAlt } from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
 
@@ -7,6 +12,11 @@ function Login() {
         email: '',
         password: '',
     })
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth)
 
     const {email, password} = formData
 
@@ -17,8 +27,35 @@ function Login() {
         }))
     }
 
+    useEffect(() => {
+
+        if(isError) {
+            toast.error(message)
+        }
+
+        if(isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData))
+    }
+
+    
+
+    if(isLoading) {
+        return <Spinner />
     }
 
   return (
